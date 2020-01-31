0b9aeccd3c89c593ccfec56fa9b603de1cc84729
serfin <serfin009@gmail.com>
2020-01-31 01:12:29 +0100

    Added basic communication

--- src/BeComfy.Services.SignalR/Hubs/ClientProcessHub.cs
+++ src/BeComfy.Services.SignalR/Hubs/ClientProcessHub.cs
@@ -1,7 +1,15 @@
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
 namespace BeComfy.Services.SignalR.Hubs
 {
-    public class ClientProcessHub// : Hub
+    public class ClientProcessHub : Hub
     {
-        
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
--- src/BeComfy.Services.SignalR/Startup.cs
+++ src/BeComfy.Services.SignalR/Startup.cs
@@ -1,25 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using BeComfy.Services.SignalR.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace BeComfy.Services.SignalR
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddSignalR();
+            services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,14 +23,18 @@ namespace BeComfy.Services.SignalR
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
+            app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/Home", async context =>
                 {
                     await context.Response.WriteAsync("BeComfy SignalR Homepage");
                 });
+                
+                endpoints.MapHub<ClientProcessHub>("/processHub");
             });
         }
     }
