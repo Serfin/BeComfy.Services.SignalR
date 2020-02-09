e96ea24bbf87cb339c726a9681d932f849101083
serfin <serfin009@gmail.com>
2020-02-09 21:25:22 +0100

    Added userGroup field

--- src/BeComfy.Services.SignalR/wwwroot/js/client/clientProcessHub.js
+++ src/BeComfy.Services.SignalR/wwwroot/js/client/clientProcessHub.js
@@ -11,21 +11,33 @@ document.getElementById("connect").addEventListener("click", function (event) {
         if (jwt === "")
             return;
     
-        connection.invoke("Authenticate", jwt);
+        connection.invoke("InitializeAsync", jwt);
     }).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
 
-connection.on("connected", function () {
+connection.on("connected", function (userGroup) {
     var li = document.createElement("li");
     li.textContent = "User connected!";
     document.getElementById("messagesList").appendChild(li);
+
+    document.getElementById("userGroup").append(userGroup);
 })
 
 connection.on("disconnected", function () {
     var li = document.createElement("li");
     li.textContent = "User disconnected!";
+    document.getElementById("messagesList").appendChild(li);
+})
+
+connection.on("operation_result_rejected", function (operationResult, data) {
+    var li = document.createElement("li");
+    li.textContent = JSON.stringify(operationResult);
+    document.getElementById("messagesList").appendChild(li);
+
+    var li = document.createElement("li");
+    li.textContent = JSON.stringify(data);
     document.getElementById("messagesList").appendChild(li);
 })
