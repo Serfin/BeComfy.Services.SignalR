a55440352e5a810a3391eb1f2c6e86a630ec76e4
serfin <serfin009@gmail.com>
2020-02-23 16:57:03 +0100

    Added seats field

--- src/BeComfy.Services.SignalR/Operations/Messages/Tickets/TicketBought.cs
+++ src/BeComfy.Services.SignalR/Operations/Messages/Tickets/TicketBought.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using BeComfy.Common.CqrsFlow;
 using BeComfy.Common.Messages;
+using BeComfy.Common.Types.Enums;
 using Newtonsoft.Json;
 
 namespace BeComfy.Services.SignalR.Operations.Messages.Tickets
@@ -11,13 +13,16 @@ namespace BeComfy.Services.SignalR.Operations.Messages.Tickets
         public Guid Id { get; }
         public Guid CustomerId { get; }
         public decimal TotalPrice { get; }
+        public IDictionary<SeatClass, int> AvailableSeats { get; }
 
         [JsonConstructor]
-        public TicketBought(Guid id, Guid customerId, decimal totalPrice)
+        public TicketBought(Guid id, Guid customerId, decimal totalPrice,
+            IDictionary<SeatClass, int> availableSeats)
         {
             Id = id;
             CustomerId = customerId;
             TotalPrice = totalPrice;
+            AvailableSeats = availableSeats;
         }
     }
 }
